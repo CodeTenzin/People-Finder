f203e46f69eb49add497dcf074e76119f5e87be4
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-01-27 02:48:12 -0500

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+}
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
