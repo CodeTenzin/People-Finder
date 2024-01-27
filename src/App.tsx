e8854c31d700abc5835f1bf3e217fdbb87b106ad
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-01-27 03:44:22 -0500

    Setup endpoint to set users.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 interface User {
   id: number;
   name: string;
@@ -12,7 +15,45 @@ interface User {
 }
 
 const App = () => {
-  return <div>App</div>;
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof cancelIdleCallback) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    // .finally(() => {
+    //   setLoading(false);
+    // });
+    return () => controller.abort();
+  }, []);
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group">
+        {users.map((user) => (
+          <li className="list-group-item" key={user.id}>
+            {user.name} {user.email} {user.address.street} {user.address.suite}{" "}
+            {user.address.city} {user.address.zipcode} {user.phone}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default App;
